6a6afb8ee55ec1dcafdef21f374f74e78c61dd63
Tânia Esteves <ainatheidi@outlook.com>
2018-01-18 22:05:19 +0000

    Update populate_db_sinais_transito.py

--- Code/Model/createdb/populate_db_sinais_transito.py
+++ Code/Model/createdb/populate_db_sinais_transito.py
@@ -15,31 +15,18 @@ except:
 
 cur = con.cursor()
 
-try:
-    cur.execute("SELECT id from catalog where title='" + catalog_title + "';")
-except:
-    print("\nERROR: UNABLE TO GET CATALOG ID")
-    exit()
-
-rows = cur.fetchall()
-
-if rows == []:
-    print("\nERROR: CATALOG NAME INVALID")
-    exit()
-
-catalog_id = rows[0][0]
-
 counter = 0
 
 try:
 
 	cur.execute("INSERT INTO client (name, description) VALUES ('Miguel Bandeira', 'Vereador da C.M. Braga');")
 
-	cur.execute("INSERT INTO catalog (title, description) VALUES ('Sinais de Transito', 'Sinais rodoviarios da cidade de Braga');")
+	cur.execute("INSERT INTO catalog (title, description) VALUES ('Sinais de Transito', 'Sinais rodoviarios da cidade de Braga'); RETURNING id;")
+	catalogid = cursor.fetchone()[0]
 
 	cur.execute("INSERT INTO client_catalog VALUES (1, 1, 'Contracto Atlas Innovation - CMBraga');")
 
-	# features sinais de trânsito
+	-- features sinais de trânsito
 
 	cur.execute("INSERT INTO feature (code, title, icon, pathtotrainingdataset) VALUES ('A1A', 'A1A - Curva à direita', 'https://github.com/taniaesteves/UCE15/blob/master/Code/Model/Catalogs/sinais_de_transito/icons/A1A.png?raw=true', 'trainingdataset.tfrecord') RETURNING id;")
 	id_of_new_row = cursor.fetchone()[0]
